c6625f875257785da521f71ef90996ec207a0db4
Michael Owuor <michael.owuor@gmail.com>
2022-08-23 17:02:50 +0300

    feat: create slider and lazy load images

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-import Hero from '../components/hero';
 import Meta from '../lib/meta';
+import Hero from '../components/hero';
+import Slider from '../components/slider';
+import { SliderData } from '../components/slider-data';
 
 const Home = () => {
 	return (
@@ -8,7 +10,13 @@ const Home = () => {
 			<Meta title="Captur Photography | Superior Software Solutions" />
 			{/** title bar end */}
 
+			{/** hero section start */}
 			<Hero heading="Captur Photography" message="I capture moments in nature and keep them alive" />
+			{/** hero section end */}
+
+			{/** slider section start */}
+			<Slider slides={SliderData} />
+			{/** slider section end */}
 		</>
 	);
 };
